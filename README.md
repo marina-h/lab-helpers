e9b8466112e5b4bf646ff3d3dc80b7991353da72
Marina Hoashi <marinahoashi@gmail.com>
2013-08-07 12:02:14 -0400

    first version of primer_helper

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ My practice projects
 primer_helper.py:
 
 This is a simple python script that will:
+	
 	1. calculate basic melting temperature
+	
 	2. print the reverse complement
 	
 
--- primer_helper.py
+++ primer_helper.py
@@ -1,8 +1,39 @@
 '''
 1. Asks for primer sequence
-2. Returns Tm (simple calculation)
-3. Returns reverse complement
+2. Returns Tm (simple calculation) and reverse complement
 '''
 
-primer = input("What is the primer sequence?\n> ")
-print ("Your primer sequence is: %s" % primer)
+primer = input("\nWhat is the primer sequence?\n> ").upper()
+
+# add assertion for ATGC, all caps
+
+def melting_temp(p):
+	Tm = 0
+	for n in p:
+		if n == 'A' or n == 'T':
+			Tm += 2
+		if n == 'G' or n == 'C':
+			Tm += 4
+	return Tm 
+
+
+reverse_comp_dict = {'A':'T', 'T':'A','G':'C', 'C':'G'}
+def reverse_comp(p):
+	reverse = ""
+	for n in p[::-1]:
+		reverse += reverse_comp_dict[n]
+	return reverse
+
+print ("\nYour primer sequence is:\n5' %s (%s bp)\n" % (primer, len(primer)))
+print ("Its reverse complement is:\n5'", reverse_comp(primer), "\n")
+print ("The Tm of this primer (A,T=2°C, G,C=4°C) is:\n",melting_temp(primer), "°C")
+print ("\n")
+
+
+
+
+# print
+# '''
+# 
+# '''
+# selection = input("> ")
