dcd3e71e84c84521c20d488710fa15b689c9cd0f
Marina Hoashi <marinahoashi@gmail.com>
2013-08-13 17:51:56 -0400

    completed first version

--- wobble_maker/wobble_maker.py
+++ wobble_maker/wobble_maker.py
@@ -1,10 +1,9 @@
-from collections import OrderedDict
-
-# ask for primer sequence with flanking "/"s
-primer = raw_input("\nWhat is your primer sequence?                      \
-					\nPlease flank the codon containing the point mutation with '/' symbols, \
-					\nand insert spaces between codons. \
-					\nExample: AAA TTT /GGG/ CCC GGG \
+# ask for primer sequence (in specified format)
+print "\n------------------------------------------------------------------------"
+primer = raw_input("\nWhat is the sequence of the primer you want to make wobble changes in? \
+					\nPlease flank the codon containing the point mutation with '/' symbols \
+					 and insert spaces between codons. \
+					\nExample: 'AAA TTT /GGG/ CCC GGG' without the quotes. \
 					\n> ").upper()
 
 # this code was found online
@@ -16,11 +15,8 @@ codon_table = dict(zip(codons, amino_acids))
 # split primer seq into codons based on spaces and annotate with protein codes
 def pretty_protein_code(split_primer):
 	protein_codes = ' '
-	# keep track of non-codons at edges of primer
-	n_of_noncodons = 0
 	for chunk in split_primer:
 		if len(chunk) < 3:
-			n_of_noncodons += 1
 			protein_codes += '-' * len(chunk) + ' '
 		elif '/' in chunk:
 			protein_codes += '  ' + codon_table[chunk[1:4]] + '   '
@@ -28,25 +24,16 @@ def pretty_protein_code(split_primer):
 			protein_codes += ' ' + codon_table[chunk] + '  '
 	return protein_codes
 
-print "\nYour primer sequence and its protein translation is:\n5'", primer
-primer_translation = pretty_protein_code(primer.split())
-print " ", primer_translation
-
-# ask for number of changes to be made on right and left side
-left_changes  = int(raw_input("\nHow many wobble changes do you want to the left? \
-	                       \n> "))
-right_changes = int(raw_input("\nTo the right? \
-	                       \n> "))
-
-# Summarize input about wobble changes to be made
-print "\nYou want %s wobble change(s) on the right, and %s wobble change(s) on the left.\n" % (
-	  left_changes, right_changes)
-
-# make dict of codons:AA of primer excluding incomplete codons
-def codons_only(p):
-	return [n for n in p.split(' ') if len(n) >= 3]
-
-## primer_dict = OrderedDict(zip(codons_only(primer), primer_translation.split()))
+# ask for number of changes to be made on right and left side and summarize
+while True:
+	left_changes  = int(raw_input("\nHow many wobble changes do you want to the left of the mutation?\n> "))
+	right_changes = int(raw_input("\nTo the right?\n> "))
+	print "\nYou want %s wobble change(s) to the right, and %s wobble change(s) to the left." % (
+		  left_changes, right_changes)
+	print "Is this correct? (Y/N)", 
+	answer = raw_input("\n> ").upper()
+	if answer == "Y" or answer == "YES":
+		break
 
 # find postion of pt mutation
 def find_pt_mutation_pos(l):
@@ -59,9 +46,10 @@ def find_pt_mutation_pos(l):
 		else:
 			print "Error: Primer does not specify locaiton of pt mutation with '/'"
 
+primer_translation = pretty_protein_code(primer.split())
 AA_list = primer_translation.split()
 
-# mutate wobble positions (next codon with same AA in codon_table)
+# mutate wobble positions (next codon with same AA and same 1st two letters in codon_table)
 def wobble(primer):
 	mut_codon_list = primer.split()
 	pos = find_pt_mutation_pos(mut_codon_list)
@@ -83,6 +71,13 @@ def wobble(primer):
 				mut_codon_list[pos-start_l] = c
 				break
 		start_l += 1
+	
 	return mut_codon_list
 
-print wobble(primer)
+# print out new and old primers with their translations
+print "\nYour original primer sequence and its protein translation is:\n5'", primer
+print " ", primer_translation
+
+new_primer = " ".join(wobble(primer))
+print "\nYour new primer sequence and its protein translation is:\n5'", new_primer
+print " ", pretty_protein_code(new_primer.split()), "\n"
